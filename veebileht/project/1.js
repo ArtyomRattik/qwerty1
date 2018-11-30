2297b41db542e48b46506f42e68d431dd09e6b7f
Feigsen <44437636+Feigsen@users.noreply.github.com>
2018-11-30 22:49:05 +0200

    Add files via upload

---
+++ veebileht/project/1.js
@@ -0,0 +1,10 @@
+document.getElementById('slider-left').onclick = sliderLeft;
+var left = 0;
+function sliderLeft() {
+    var polosa = document.getElementById('polosa');
+    left = left - 200;
+    if (left < -600) {
+        left = 0;
+    }
+    polosa.style.left = left+'px';
+}
